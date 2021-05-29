717804a69ead66024ba2f039119b67b1af6095aa
Hira Fatima <hi.fatimaa@outlook.com>
2021-05-29 16:52:31 -0400

    empty constructor plus intialization of features

--- src/test/Employee.java
+++ src/test/Employee.java
@@ -3,4 +3,13 @@ package src.test;
 public class Employee {
     public String name;
     public int id;
+
+    public Employee (){
+
+    }
+
+    public Employee (String name, int id){
+        this.name = name;
+        this.id = id;
+    }
 }
